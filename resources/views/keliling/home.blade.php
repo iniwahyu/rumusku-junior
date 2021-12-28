780c8c725d8da6fd85db4ce96cc0a16c27fe563e
Wahyu Rizky <wrep17@gmail.com>
2021-12-28 16:34:50 +0700

    Add: List Luas dan Keliling Bangun Datar

--- resources/views/keliling/home.blade.php
+++ resources/views/keliling/home.blade.php
@@ -1,1 +1,105 @@
 @extends($defaultTemplate)
+
+@section('css-library')
+    
+@endsection
+
+@section('css-custom')
+    
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Keliling Bangun Datar</h4>
+            </div>
+            <div class="card-body"> 
+                <div class="row">
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/persegi') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Persegi</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/persegi-panjang') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Persegi Panjang</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/segitiga') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Segitiga</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/trapesium') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Trapesium</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/jajar-genjang') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Jajar Genjang</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/belah-ketupat') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Belah Ketupat</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/layangan') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Layang-Layang</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('keliling/lingkaran') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Lingkaran</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js-library')
+    
+@endsection
+
+@section('js-custom')
+    
+@endsection
--- resources/views/luas/home.blade.php
+++ resources/views/luas/home.blade.php
@@ -15,8 +15,81 @@
             <div class="card-header">
                 <h4 class="card-title">Luas Bangun Datar</h4>
             </div>
-            <div class="card-body">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur consequatur deserunt aliquam eveniet, molestias eligendi, et vitae ullam velit odio assumenda! Perferendis tempore distinctio molestias modi eveniet minima voluptate itaque.</p>
+            <div class="card-body"> 
+                <div class="row">
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/persegi') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Persegi</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/persegi-panjang') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Persegi Panjang</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/segitiga') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Segitiga</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/trapesium') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Trapesium</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/jajar-genjang') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Jajar Genjang</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/belah-ketupat') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Belah Ketupat</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/layangan') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Layang-Layang</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4">
+                        <a href="{{ url('luas/lingkaran') }}">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h3>Lingkaran</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
