8e8db24cebea9480d786917610e1994740098c97
Wahyu Rizky <wrep17@gmail.com>
2022-01-06 21:35:54 +0700

    Add: Sidebar Percentage

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -91,6 +91,14 @@
                                 </p>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a href="{{ url('presentase') }}" class="nav-link">
+                                <i class="nav-icon fas fa-list"></i>
+                                <p>
+                                    Percentage
+                                </p>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
